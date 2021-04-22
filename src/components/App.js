908ac54539b478c9a9ba6f6cc5d9b30d73beb2ad
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-22 10:08:39 -0300

    Added a Header component to show the introductory text.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "../styles/App.css";
 
+function Header() {
+  return (
+    <div className="header">
+      <h3>Simple, traffic-based pricing</h3>
+      <span>
+        <p>Sign-up for our 30-day trial.</p>
+        <p>No credit card required.</p>
+      </span>
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <div className="price-box">
+          <div className="slider"></div>
+          <div className="toggle"></div>
+          <div className="info"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
