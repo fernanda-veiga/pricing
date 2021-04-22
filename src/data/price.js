52d99c9866717a03111eca347842771643cf35bf
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-22 17:52:43 -0300

    Added a price object with prices for each pageview number.

---
+++ src/data/price.js
@@ -0,0 +1,9 @@
+const price = {
+  1: { pageviews: "10K", value: 8 },
+  2: { pageviews: "50K", value: 12 },
+  3: { pageviews: "100K", value: 16 },
+  4: { pageviews: "500K", value: 24 },
+  5: { pageviews: "1M", value: 36 },
+};
+
+export default price;
