dbe21281984e05b8274dc370fa3217eeec353de0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-22 15:33:14 -0300

    Added components to display the pricing using a range slider.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/App.css";
 
 function Header() {
@@ -13,17 +13,58 @@ function Header() {
   );
 }
 
+function PricingBox() {
+  return (
+    <div className="pricing-box">
+      <SliderArea />
+      <div className="toggle-area"></div>
+      <div className="info-area"></div>
+    </div>
+  );
+}
+
+function SliderArea() {
+  return (
+    <div className="slider-area">
+      <p className="pageviews">100K PAGEVIEWS</p>
+      <RangeSlider />
+      <span>
+        <p className="price">$16.00</p>
+        <p className="frequency">/month</p>
+      </span>
+    </div>
+  );
+}
+
+function RangeSlider() {
+  useEffect(() => {
+    document.querySelector("input").value = 3;
+  }, []);
+
+  function handleSlide(event) {
+    const percentage =
+      ((event.target.value - 1) * 100) / (event.target.max - event.target.min);
+    event.target.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0% ${percentage}%, hsl(224, 65%, 95%) ${percentage}% 100%`;
+  }
+
+  return (
+    <div className="slider-container">
+      <input
+        type="range"
+        min="1"
+        max="5"
+        className="slider"
+        onInput={handleSlide}
+      />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Header />
-        <div className="price-box">
-          <div className="slider"></div>
-          <div className="toggle"></div>
-          <div className="info"></div>
-        </div>
-      </div>
+      <Header />
+      <PricingBox />
     </div>
   );
 }
